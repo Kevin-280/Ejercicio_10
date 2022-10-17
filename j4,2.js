679a638834ca32c7eb400385acd111c9d8f78504
kevin-280 <kevinjime26@gmail.com>
2022-10-16 20:26:33 -0500

    archvos subidos

--- j4,2.js
+++ j4,2.js
@@ -1,29 +1,36 @@
 //Escribir un programa que imprima los horarios en que sebe tomar los medicamentos un paciente. 
 //solicitar al usuario la horaa de la toma inicial (0-23) y la frecuencia de la toma diaria (1-12)
 
+let HoraInicio = new Date();
+let TomaMed = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10','11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24']
 
-console.log("ingrese la hora incial");
-let horario = Number(prompt("ingrese la hora de toma: "))
+let Hora = HoraInicio.getDay();
 
-console.log("Frecuencia");
-let frecuencia = Number(prompt("ingrese la frecuencia: "))
+console.log(`Horarios: ${TomaMed[Hora]}`);
 
-if (horario > 5) {
-    let suma_total = horario + frecuencia;
-    console.log(suma_total)
-}
-else if (horario > 11) {
-    let suma_total = horario + frecuencia;
-    console.log(suma_total)
-}
-else if (horario > 17){
-    let suma_total = horario + frecuencia;
-    console.log(suma_total)
-}
-else if (horario > 23){
-    let suma_total = horario + frecuencia;
-    console.log(suma_total)
-}
-else {
-    console.log("No hay horario ")
-};
+console.log();
+
+let Hora1 = HoraInicio.getHours();
+let Hora2 = HoraInicio.getHours();
+let Hora3 = HoraInicio.getHours();
+let Hora4 = HoraInicio.getHours();
+
+let Tiempo1 = Hora1 >= 0 ? '1' : '0';
+let Time1 = Hora2 >= 0 ? '5' : '0';
+let Tiempo2 = Hora1 >= 0 ? '2' : '0';
+let Time2 = Hora2 >= 0 ? '11' : '0';
+let Tiempo3 = Hora1 >= 0 ? '3' : '0';
+let Time3 = Hora2 >= 0 ? '17' :  '0';
+let Tiempo4 = Hora1 >= 0 ? '4' : '0';
+let Time4 = Hora2 >= 0 ? '23' : '0';
+
+console.log(`${Time1 % 23} ${Tiempo1} : ${Time1}`);
+console.log(`${Time1 % 23} ${Tiempo2} : ${Time2}`);
+console.log(`${Time1 % 23} ${Tiempo3} : ${Time3}`);
+console.log(`${Time1 % 23} ${Tiempo4} : ${Time4}`);
+
+let FrecuenciaInicio = new Date();
+let TomaMedInicio = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10','11', '12',];
+
+let Tomar = FrecuenciaInicio.getDay();
+console.log(`La frecuencia es: ${TomaMedInicio[Hora]}`);
--- j4.js
+++ j4.js
@@ -3,11 +3,48 @@
 //el mismo da como resultado el mismo numero.
 //ejemplo: para el 6 sus diversos son 1,2,3,6 sumando todas excepto el 6: 1+2+3 = 6,6 es perfecto
 
-console.log("Inserte un numero");
-let num = Number(prompt("ingrese un numero"))
 
-if (num >= 1 ) {
-    console.log("Excelente")
-} else {
-    console.log("No Excelente")
-};
+
+let cant_numeros = 4;
+
+
+let num_divisores = [];
+
+
+let numeros_perfectos = [];
+
+
+let numero = 1;
+
+while (true) {
+
+    
+    for (let divisor = 1; divisor < numero; divisor++) {
+        if (numero%divisor == 0) {
+            num_divisores.push(divisor);
+        }
+    }
+
+    let sum = 0;
+    for (let index = 0; index < num_divisores.length; index++) {
+        sum = num_divisores[index] + sum;
+    }
+
+    if (sum == numero) {
+        
+        numeros_perfectos.push(numero);
+    }
+
+    if (numeros_perfectos.length == cant_numeros) {
+        break
+    }
+    
+    num_divisores = [];
+
+  
+    numero = numero + 1;
+}
+console.log("La cantidad de numeros perfectos solicitados fue de: " + cant_numeros + " y son los siguientes")
+console.log(numeros_perfectos);
+
+document.write(`<h2> ${numeros_perfectos} </h2>`);
